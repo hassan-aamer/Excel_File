d36c4864c4e93c3c0908d38745e75c0b65b85f4b
hassan-aamer <hassanaamer048@gmail.com>
2023-10-25 04:42:49 +0300

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\UserExcel::factory(50)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
